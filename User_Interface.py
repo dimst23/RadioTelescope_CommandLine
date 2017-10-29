14f81b954be3863dcf159c217de892d0367f50af
Dimitrios <dstoupis@auth.gr>
2017-10-29 20:41:22 +0000

    Added a new function which helps in the location menu and it gets the coordinates from the user. Also added some more functionality to the location menu. Finally the clear screen functions were rearanged to create a better result.

--- User_Interface.py
+++ User_Interface.py
@@ -23,30 +23,47 @@ class uInterface(object):
 		choice = input()
 		
 		if choice == "2":
-			self.cls() #Clear the previous menu before showing the new one
 			self.objectMenu()
 		elif choice == "3":
-			self.cls() #Clear the previous menu before showing the new one
 			self.transitMenu()
 		elif choice == "5":
-			self.cls() #Clear the previous menu before showing the new one
 			self.TCPMenu()
 		elif choice == "6":
-			self.cls() #Clear the previous menu before showing the new one
 			self.locationMenu()
 
 	def locationMenu(self):
-		#Show current settings on the top. That will be done latter after deciding for the final menu outline
-		print("Location Menu:")
-		print("1. Keep current location")
-		print("2. Set new location")
-		print("3. Return to main menu")
-		print("Enter your menu choice: ")
-		
-		choice = input()
-		#Control to be added
+		wrong_ch = False
+		while(True):
+			self.cls() #Clear the previous menu before showing the new one
+			#Show current settings on the top. That will be done latter after deciding for the final menu outline
+			print("Location Menu:")
+			print("1. Set new location")
+			print("2. Return to main menu")
+			if wrong_ch == True:
+				print("Enter a correct number please: ")
+			else:
+				print("Enter your menu choice: ")
+			
+			choice = input()
+			#Control to be added
+			if choice == "1":
+				self.cls() #Clear the screen for the new menu
+				lat = self.setAngle("Latitude")
+				print("Latitude is set to: " + lat)
+				lon = self.setAngle("Longitude")
+				print("Longitude is set to: " + lon)
+				print("Do you accept the values? If yes type 'y', otherwise type anything:")
+				acc = input()
+				if acc == "y":
+					break
+			elif choice == "2":
+				break
+			else:
+				wrong_ch = True
 
 	def TCPMenu(self):
+		self.cls() #Clear the previous menu before showing the new one
+		#Show current settings for TCP. This will be enabled once the clisent code and config file code has been written
 		print("TCP settings menu:")
 		print("1. Change host")
 		print("2. Chage port")
@@ -58,6 +75,7 @@ class uInterface(object):
 		#Control to be added
 
 	def transitMenu(self):
+		self.cls() #Clear the previous menu before showing the new one
 		print("Transit menu:")
 		print("1. Provide transit point coordinates (Dec/RA)")
 		print("2. Choose time for the currently chosen object")
@@ -68,6 +86,7 @@ class uInterface(object):
 		#Control to be added
 	
 	def objectMenu(self):
+		self.cls() #Clear the previous menu before showing the new one
 		print("Object choice menu:")
 		print("1. Sun")
 		print("2. Moon")
@@ -77,6 +96,14 @@ class uInterface(object):
 		
 		choice = input()
 		#Control to be added
+	
+	def setAngle(self, angName):
+		print("\nEnter the " + angName + " in decimal degrees.")
+		print("For a south Latitude or a weste Longitude, enter a minus sign in the decimal value.")
+		print(angName + ":")
+		ang = input()
+		
+		return ang
 	
 	def cls(self):
 		os.system('cls' if os.name == 'nt' else 'clear')
