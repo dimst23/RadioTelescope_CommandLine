222833b9d75c6c267c5940ef399835569d52c41a
Dimitrios <dstoupis@auth.gr>
2017-10-30 07:28:25 +0000

    Rearanged the input function and included the prompt string in the input() function argument. Also added some functionality to TCPMenu and some comments throughout the code. Finally changed some user prompt strings.

--- User_Interface.py
+++ User_Interface.py
@@ -10,26 +10,25 @@ class uInterface(object):
 		print("***********************************************")
 	
 	def mainMenu(self):
-		self.cls() #Clear the previous menu before showing the new one
-		print("Main Menu:")
-		print("1. Read Position")
-		print("2. Choose object")
-		print("3. Transit")
-		print("4. Aim and guide")
-		print("5. TCP settings")
-		print("6. Location settings")
-		print("Enter your menu choice: ")
-		
-		choice = input()
-		
-		if choice == "2":
-			self.objectMenu()
-		elif choice == "3":
-			self.transitMenu()
-		elif choice == "5":
-			self.TCPMenu()
-		elif choice == "6":
-			self.locationMenu()
+		while(True):
+			self.cls() #Clear the previous menu before showing the new one
+			print("Main Menu:")
+			print("1. Read Position")
+			print("2. Choose object")
+			print("3. Transit")
+			print("4. Aim and guide")
+			print("5. TCP settings")
+			print("6. Location settings")
+			choice = input("Enter your menu choice: ")
+			
+			if choice == "2":
+				self.objectMenu()
+			elif choice == "3":
+				self.transitMenu()
+			elif choice == "5":
+				self.TCPMenu()
+			elif choice == "6":
+				self.locationMenu()
 
 	def locationMenu(self):
 		wrong_ch = False
@@ -39,12 +38,11 @@ class uInterface(object):
 			print("Location Menu:")
 			print("1. Set new location")
 			print("2. Return to main menu")
-			if wrong_ch == True:
-				print("Enter a correct number please: ")
+			if wrong_ch:
+				choice = input("Enter a correct number please: ")
 			else:
-				print("Enter your menu choice: ")
+				choice = input("Enter your menu choice: ")
 			
-			choice = input()
 			#Control to be added
 			if choice == "1":
 				self.cls() #Clear the screen for the new menu
@@ -52,8 +50,7 @@ class uInterface(object):
 				print("Latitude is set to: " + lat)
 				lon = self.setAngle("Longitude")
 				print("Longitude is set to: " + lon)
-				print("Do you accept the values? If yes type 'y', otherwise type anything:")
-				acc = input()
+				acc = input("Do you accept the values? If yes type 'y', otherwise type anything: ")
 				if acc == "y":
 					break
 			elif choice == "2":
@@ -62,17 +59,40 @@ class uInterface(object):
 				wrong_ch = True
 
 	def TCPMenu(self):
-		self.cls() #Clear the previous menu before showing the new one
-		#Show current settings for TCP. This will be enabled once the clisent code and config file code has been written
-		print("TCP settings menu:")
-		print("1. Change host")
-		print("2. Chage port")
-		print("3. Test current connection")
-		print("4. Return to main menu")
-		print("Enter your menu choice: ")
-		
-		choice = input()
-		#Control to be added
+		wrong_ch = False
+		while(True):
+			self.cls() #Clear the previous menu before showing the new one
+			#Show current settings for TCP. This will be enabled once the clisent code and config file code has been written
+			print("TCP settings menu:")
+			print("1. Change host")
+			print("2. Chage port")
+			print("3. Test current connection")
+			print("4. Return to main menu")
+			choice = input("Enter your menu choice: ")
+			
+			#Control to be added
+			if choice == "1":
+				self.cls() #Clear the previous menu before showing the new one
+				print("\nEnter the server host name e.g. \"localhost\" or 127.0.0.1:")
+				host = input() #Get the host name from the user
+				print("The entered host is: " + host)
+				continue #Stay in the TCP menu
+			elif choice == "2":
+				while(True):
+					self.cls() #Clear the previous menu before showing the new one
+					if wrong_ch:
+						print("Please give an integer as the server port: ")
+					else:
+						print("\nEnter the server port as an integer e.g. 10001: ")
+					try:
+						port = int(input("Enter port: "), 10)
+						break
+					except:
+						wrong_ch = True
+						continue
+				continue #Stay in the TCP menu
+			elif choice == "4":
+				break
 
 	def transitMenu(self):
 		self.cls() #Clear the previous menu before showing the new one
@@ -80,9 +100,8 @@ class uInterface(object):
 		print("1. Provide transit point coordinates (Dec/RA)")
 		print("2. Choose time for the currently chosen object")
 		print("3. Return to main menu")
-		print("Enter your menu choice: ")
+		choice = input("Enter your menu choice: ")
 		
-		choice = input()
 		#Control to be added
 	
 	def objectMenu(self):
@@ -92,18 +111,14 @@ class uInterface(object):
 		print("2. Moon")
 		print("3. Jupiter")
 		print("4. Return to main menu")
-		print("Enter your menu choice: ")
+		choice = input("Enter your menu choice: ")
 		
-		choice = input()
 		#Control to be added
 	
 	def setAngle(self, angName):
 		print("\nEnter the " + angName + " in decimal degrees.")
 		print("For a south Latitude or a weste Longitude, enter a minus sign in the decimal value.")
-		print(angName + ":")
-		ang = input()
-		
-		return ang
+		return input(angName + ": ") #Return the angle entered by the user
 	
 	def cls(self):
 		os.system('cls' if os.name == 'nt' else 'clear')
@@ -111,6 +126,5 @@ class uInterface(object):
 
 
 #Initial test code
-uInterface().cls()
 
 uInterface().mainMenu()
