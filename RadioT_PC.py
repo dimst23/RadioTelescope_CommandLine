c7ab00e7dd51fc17809c474f4668dd2cfec00079
Dimitrios <dstoupis@auth.gr>
2017-11-02 16:09:57 +0000

    Added the TCP client functionality and exception handling code.

--- RadioT_PC.py
+++ RadioT_PC.py
@@ -2,6 +2,7 @@
 
 from User_Interface import uInterface
 from configData import confData
+from TCPClient import TCPClient
 import sys
 
 if __name__ == '__main__':
@@ -11,5 +12,9 @@ if __name__ == '__main__':
 		print("There is a problem with the XML file handling.\n%s" %e)
 		sys.exit(1) #Terminate the script
 	
-	#TCP exception handling code to be added for TCP too
-	uInterface().mainMenu(cfgData)
+	try:
+		tcpClient = TCPClient(cfgData)
+	except Exception as e:
+		print("There is a problem with the TCP handling.\n%s" %e)
+		sys.exit(1) #Terminate the script
+	uInterface().mainMenu(cfgData, tcpClient)
