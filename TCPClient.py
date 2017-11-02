eef01561b1451e8261d3f940f554a09e968f4a13
Dimitrios <dstoupis@auth.gr>
2017-11-02 16:09:23 +0000

    Added some important exception handling code in TCP client and corrected a mispelled variable name.

--- TCPClient.py
+++ TCPClient.py
@@ -3,9 +3,10 @@ import socket
 class TCPClient(object):
 	def __init__(self, cfgData):
 		self.conf = cfgData
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock_exst = True
-		autocon = cfgData.getTCPAutoConnStatus()
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create a socket object
+		self.sock.settimeout(20) #Set the timeout to 20 seconds
+		self.sock_exst = True #Indicate that a socket object exists
+		autocon = cfgData.getTCPAutoConnStatus() #See if autoconnection at startup is enabled
 		if autocon == "yes":
 			host = cfgData.getHost()
 			port = cfgData.getPort()
@@ -16,11 +17,11 @@ class TCPClient(object):
 			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			self.sock_exst = True
 		try:
-			self.sock.connect((host, port))
+			self.sock.connect((host, int(port)))
 			self.sock_connected = True
 		except:
 			self.sock_connected = False
-		return self.sock.connected
+		return self.sock_connected
 	
 	def disconnect(self):
 		if self.sock_exst and self.sock_connected:
@@ -31,8 +32,12 @@ class TCPClient(object):
 			self.sock_connected = False
 	
 	def sendRequest(self, request):
-		if self.sock.connected:
-			self.sock.send(request.encode('utf-8'))
-			return self.sock.recv(1024).decode('utf-8')
+		if self.sock_connected:
+			try:
+				self.sock.send(request.encode('utf-8'))
+				response = self.sock.recv(1024).decode('utf-8')
+				return response
+			except:
+				return "No answer"
 	
 	
