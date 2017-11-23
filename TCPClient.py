c121baa2426f1e2c15f2dd702286a3cefe946f72
Dimitrios <dstoupis@auth.gr>
2017-11-23 18:25:32 +0000

    Minor changes to TCPClient file. Some reorganization of the menu items in showMenu and uploading the new settings file.

--- TCPClient.py
+++ TCPClient.py
@@ -3,21 +3,24 @@ import socket
 class TCPClient(object):
     def __init__(self, cfgData):
         self.conf = cfgData
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create a socket object
-        self.sock.settimeout(20) #Set the timeout to 20 seconds
-        self.sock_exst = True #Indicate that a socket object exists
-        self.sock_connected = False
+        self.sock_exst = False #Indicate that a socket does not object exist
+        self.sock_connected = False #Indicate that there is currently no connection
+        self.sock = self.createSocket()
         autocon = cfgData.getTCPAutoConnStatus() #See if autoconnection at startup is enabled
         if autocon == "yes":
             host = cfgData.getHost()
             port = cfgData.getPort()
             self.connect(host, port)
-            
+    
+    def createSocket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create a socket object
+        sock.settimeout(20) #Set the timeout to 20 seconds
+        self.sock_exst = True #Indicate that a socket object exists
+        return sock
+    
     def connect(self, host, port):
         if self.sock_exst == False:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.settimeout(20) #Set the timeout to 20 seconds
-            self.sock_exst = True
+            self.sock = self.createSocket()
         try:
             self.sock.connect((host, int(port)))
             self.sock_connected = True
