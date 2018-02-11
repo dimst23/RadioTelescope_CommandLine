42e8c9d1b8ce43dce993aa6f8e9f5b10eeba21c1
Dimitrios <dstoupis@auth.gr>
2018-02-11 11:18:40 +0000

    Improved the exception handling code and the user message output.

--- RadioT_PC.py
+++ RadioT_PC.py
@@ -7,29 +7,39 @@ import logData
 import sys
 
 if __name__ == '__main__':
-    logdata = logData.logData(__name__)
+    #Exception handling section for the log file code
+    try:
+        logdata = logData.logData(__name__)
+    except:
+        print("There is a problem with the handling of the log file. See log file for the traceback of the exception.\n")
+        logdata.log("EXCEPT", "There is a problem with the handling of the log file. Program terminates.", __name__)
+        exit(1) #Terminate the script
     
     #Exception handling code for the XML file process
     try:
         cfgData = configData.confData("settings.xml")
-    except Exception as e:
-        print("There is a problem with the XML file handling.\n%s" %e)
+    except:
+        print("There is a problem with the XML file handling. See log file for the traceback of the exception.\n")
         logdata.log("EXCEPT", "There is a problem with the XML file handling. Program terminates.", __name__)
         exit(1) #Terminate the script
     
     #Exception handling code for the TCP initial setup
     try:
         tcpClient = TCPClient.TCPClient(cfgData)
-    except Exception as e:
-        print("There is a problem with the TCP handling.\n%s" %e)
+    except:
+        print("There is a problem with the TCP handling. See log file for the traceback of the exception.\n")
         logdata.log("EXCEPT", "There is a problem with the TCP handling. Program terminates.", __name__)
         exit(1) #Terminate the script
     
     #General exception handling code
     try:
         User_Interface.uInterface(cfgData, tcpClient) #Initiate the user interface
+    except KeyboardInterrupt:
+        print("User requested termination with a keyboard interrupt.\n")
+        logdata.log("EXCEPT", "User requested termination with a keyboard interrupt.", __name__)
+        exit(0) #Terminate the script
     except:
         print("Something really bad happened!! We should terminate.\n")
         logdata.log("EXCEPT", "Something really bad happened!! See the traceback below.", __name__)
         exit(1) #Terminate the script
-    logdata.logClose()
+    logdata.logClose() #Terminate all logging operations before exiting the script
