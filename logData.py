82feff148f5df87499f121bc17f6e2222a6828f0
Dimitrios <dstoupis@auth.gr>
2018-02-12 21:17:14 +0000

    Added the default argument to the log function and also disabled the stream handler.

--- logData.py
+++ logData.py
@@ -3,16 +3,17 @@ import logging
 class logData(object):
     def __init__(self, loger_name):
         fileHandler = logging.FileHandler('RadioTelescope.log') #Create the necessary file
-        strmHandler = logging.StreamHandler() #Limited use, removed in the end product
+        #strmHandler = logging.StreamHandler() #Limited use, removed in the end product
+        
         #Set the formatting for the logs
         #Include level:time_of_log:name_of_logger:log_message
         fileHandler.setFormatter(logging.Formatter('%(levelname)s:%(asctime)s:%(name)s:%(message)s'))
         self.logger = logging.getLogger(loger_name) #Initialize the logger
         self.logger.setLevel(logging.INFO) #Set the logging level for the current logger
         self.logger.addHandler(fileHandler) #Add the file handler to the logger
-        self.logger.addHandler(strmHandler) #Used when debugging
+        #self.logger.addHandler(strmHandler) #Used when debugging
     
-    def log(self, level, msg, fname):
+    def log(self, level, msg, fname = ""):
         if level == "DEBUG":
             self.logger.debug(fname + ":" + msg)
         elif level == "INFO":
